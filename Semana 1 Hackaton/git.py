87784c36bd959e0fadec64fe1ee797166f69b429
yemaquna <yesenia.quispe@PachaQTec.edu.pe>
2020-05-30 18:56:45 -0500

    22 commit

--- Semana 1 Hackaton/git.py
+++ Semana 1 Hackaton/git.py
@@ -1,6 +1,1 @@
 
-**Semana 1 Hachaton**
-
-> Creacion de URL en Git Hub y Git Lab
-
-![enter image description here](https://image.slidesharecdn.com/tomacolorguia3dmaster-121011150448-phpapp01/95/toma-color-guia-3d-master-1-728.jpg?cb=1349967973)
